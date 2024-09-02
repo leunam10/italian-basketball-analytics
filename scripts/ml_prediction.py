006ef9bc88d5fd82031bb8e5dbf7df54c7759713
Manuel Colavincenzo <colavincenzo.manuel@gmail.com>
2024-09-02 22:08:16 +0000

    creation of the prediction script

---
+++ scripts/ml_prediction.py
@@ -0,0 +1,10 @@
+"""
+
+This script is used to build  machine learning models capable of doing different prediction:
+
+- team season winner
+- team partecipating to the final of the playoff
+- team partecipanting to the playoff
+- player winner of the mvp price
+
+"""
